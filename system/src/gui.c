5816bab8071d6b53ebf37f200bbe25152ee24ad0
qubitkiwi <asong1230@gmail.com>
2023-09-08 15:48:50 +0900

    fix gui

--- system/src/gui.c
+++ system/src/gui.c
@@ -10,13 +10,15 @@
 pid_t create_gui() {
     
     const char *name = "gui"; 
-    pid_t gui_pid = fork();
+    pid_t gui_pid;
+
+    gui_pid = fork();
     if (gui_pid == -1) {
-        perror("gui fork");
+        perror("gui fork err");
         exit(-1);
     } else if (gui_pid == 0) {
         if (prctl(PR_SET_NAME, (unsigned long) name) < 0) {
-            perror("prctl()");
+            perror("gui prctl() err");
         }
         gui_main();
     }
@@ -26,7 +28,7 @@ pid_t create_gui() {
 void gui_main() {
     printf("gui Process\n");
 
-	execl("/usr/bin/chromium-browser", "chromium-browser", "http://0.0.0.0:8080", NULL);
+	// execl("/usr/bin/chromium-browser", "chromium-browser", "http://0.0.0.0:8080", NULL);
 //    execl("/usr/bin/google-chrome-stable", "google-chrome-stable", "http://0.0.0.0:8000", NULL);
-    
+    exit(0);
 }
